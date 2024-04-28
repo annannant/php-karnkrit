9187710adb156a18691749c82fc1b1c23739f962
Barami Watthanakunanon <barami.wat@cpf.co.th>
2024-04-28 23:08:07 +0700

    update

--- php/src/logout.php
+++ php/src/logout.php
@@ -4,13 +4,15 @@ include ('config/db.php');
 
 $sql = "UPDATE users SET last_login_date = NOW()
 WHERE user_id = '" . $_SESSION['user'] . "';";
-
 if ($conn->query($sql)) {
   echo "Update record successfully";
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
   exit();
 }
-
 session_destroy();
-header("Location: login.php");
+?>
+
+<script type="text/javascript">
+  window.location = "/login.php";
+</script>
